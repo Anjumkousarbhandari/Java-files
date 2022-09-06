0ffbf2a63dd1f3dd816fc675c5bc5a1eff1e1e7f
Anjumkousarbhandari <112227344+Anjumkousarbhandari@users.noreply.github.com>
2022-09-06 16:17:33 +0530

    string file

---
+++ string/string.java
@@ -0,0 +1,15 @@
+class Simple {
+	public static void main(String[]args){
+	String anju = "Congratulation";
+	  System.out.println(anju.indexOf("C"));
+	  System.out.println(anju.indexOf("n"));
+	  System.out.println(anju.toUpperCase());
+	 System.out.println(anju.toLowerCase());
+	 System.out.println(anju.charAt(2));
+	 System.out.println(anju.charAt(0));
+	 System.out.println(anju.toCharArray());
+	System.out.println(anju.contains("congratulation"));
+	System.out.println(anju.valueOf(anju));
+	  
+}
+}
