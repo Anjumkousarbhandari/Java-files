2dc3e4f317901bad470929f83f0a373aaa240f0a
Anjumkousarbhandari <112227344+Anjumkousarbhandari@users.noreply.github.com>
2022-09-06 16:22:00 +0530

    condition file

---
+++ conditional.java
@@ -0,0 +1,16 @@
+class Blackmail {
+	public static void main ( String[] args ) {
+
+	short expected = 5000;
+	short actual = 5500;
+	if ( expected < actual ) {
+	System.out.println("Not spread the secrete");
+	} else {
+		System.out.println("spread the secrete");
+
+	
+	}
+	}
+}
+
+
