46694ce795de41b75e9f5644ea92baf3038058f0
Anjumkousarbhandari <112227344+Anjumkousarbhandari@users.noreply.github.com>
2022-09-06 16:21:14 +0530

    default file

---
+++ Default values/Array/array.java
@@ -0,0 +1,29 @@
+class Education {
+ public static void main (String[]args){
+	char name1 = 'A';
+	char name2 = 'N';
+	char name3 = 'J';
+	char name4 = 'J';
+	char name5 = 'M';
+	
+System.out.println (name1 +""+ name2 +""+ name3 +""+ name4 +""+ name5);
+
+	char [] adress = {'b','h','a','n','d','a','r','i');
+	System.out.println (adress);
+
+	float [] cgpa = {7.4f,7.9f,8.5f,8.9f,9.2f);
+	System.out.println (cgpa);
+
+	int [] numbers = {'1','2','3','4','5','6','7');
+	System.out.println (numbers);
+
+	long [] accountNo = {'0','8','0','5','2','2','1','2');
+	System.out.println (accountNo);
+
+	boolean [] isGoodMArks = {true};
+	System.out.println (isGoodMarks);
+
+	double [] percentage = {88.5d,72d,93.2d,59.45d);
+
+	}
+}
---
+++ Default values/default.java
@@ -0,0 +1,27 @@
+class Study {
+
+ 	static byte age;
+	static short USN;
+	static int carNo;
+	static long adhaarNol;
+	static float weight;
+	static double aggregate;
+	static boolean isGood;
+	static char A;
+	
+	public static void main (String[]args) {
+
+	 System.out.println ("Default value for byte: " + age);
+         System.out.println ("Default value for short: " + USN);
+	 System.out.println ("Default value for int: " + carNo);
+	 System.out.println ("Default value for long: " + adhaarNol);
+	 System.out.println ("Default value for float: " + weight);
+	 System.out.println ("Default value for double: " + aggregate);
+	 System.out.println ("Default value for boolean: " + isGood);
+	 System.out.println ("Default value for char: " + A);
+	}
+}
+	
+	
+	
+	
