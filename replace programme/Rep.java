ebe240f8feaebd172e5e474f743879267bc9efdf
Anjumkousarbhandari <112227344+Anjumkousarbhandari@users.noreply.github.com>
2022-09-06 17:09:20 +0530

    replace file

---
+++ replace programme/Rep.java
@@ -0,0 +1,10 @@
+class Replace{
+
+	public static void main (String [] args){
+	String Name = "every day is another chance to change your life";
+
+	String replaceString = Name.replace('e','a');
+	System.out.println(replaceString);
+
+	}
+}
---
+++ replace programme/Replace.java
@@ -0,0 +1,10 @@
+class ReplaceProgramme{
+
+	public static void main (String [] args){
+	String anju = "Anjum likes her dad a lot";
+
+	String replaceString = anju.replace('a','e');
+	System.out.println(replaceString);
+
+	}
+}
