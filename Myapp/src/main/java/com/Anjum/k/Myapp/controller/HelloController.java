9a068a8860579554a8cfe8b016c72363d93d5b68
Anjumkousarbhandari <112227344+Anjumkousarbhandari@users.noreply.github.com>
2022-12-08 18:58:33 +0530

    Spring mvc

---
+++ Myapp/src/main/java/com/Anjum/k/Myapp/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.Anjum.k.Myapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+	
+	@RequestMapping(value="/hello", method=RequestMethod.GET)
+	public String hello () {
+		System.out.println("Hello,Have a nice day");
+		return "index.jsp";
+	}
+}
---
+++ Myapp/src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h2>Hello everybody!</h2>
+
+	<form action="hello">
+	<input type="submit" value="submit">
+	
+	</form>
+</body>
+</html>
